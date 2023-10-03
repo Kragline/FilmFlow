703df8891459b235660b21c3ba50576865003d0c
Kragline <armanaraqelyan1107@gmail.com>
2023-10-03 11:32:38 -0700

    some html stuff, custom filters for movie page

--- django_movies/mainapp/templatetags/mainapp_tags.py
+++ django_movies/mainapp/templatetags/mainapp_tags.py
@@ -12,3 +12,8 @@ def colored_stars_range(number):
 @register.simple_tag()
 def other_stars_range(number):
     return range(10-number)
+
+
+@register.filter(name='format_money')
+def format_money(value):
+    return f'{ value / 1000000 } million $'
