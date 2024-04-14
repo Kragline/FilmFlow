230a22ea3a3ec7fd811deaa42d5c17b3cd735300
Kragline <armanaraqelyan1107@gmail.com>
2024-04-14 16:05:50 -0700

    movie rating final version + new movie cards

--- django_movies/mainapp/static/mainapp/js/rating.js
+++ django_movies/mainapp/static/mainapp/js/rating.js
@@ -1,5 +1,33 @@
 const ratingForm = document.getElementById('rating_form');
 
 function submitRatingForm() {
-    ratingForm.submit();
+  ratingForm.submit();
 }
+
+const labels = document.querySelectorAll('.movie-rating-label');
+
+labels.forEach(label => {
+    label.addEventListener('mouseover', () => {
+    const rating = parseInt(label.previousElementSibling.value);
+
+    for (let i = 0; i <= rating - 1; i++) {
+      const previousLabel = labels[i];
+      const starIcon = previousLabel.querySelector('.bi-star');
+
+      starIcon.classList.add('bi-star-fill');
+      starIcon.classList.remove('bi-star');
+    }
+    });
+
+    label.addEventListener('mouseout', () => {
+    const rating = parseInt(label.previousElementSibling.value);
+
+    for (let i = 0; i <= rating - 1; i++) {
+      const previousLabel = labels[i];
+      const starIcon = previousLabel.querySelector('.bi-star-fill');
+
+      starIcon.classList.add('bi-star');
+      starIcon.classList.remove('bi-star-fill');
+    }
+    });
+});
